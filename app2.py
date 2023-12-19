551231c33778903fe61ebc04552b29021a22256b
haidang7204 <154289880+haidang7204@users.noreply.github.com>
2023-12-19 15:20:17 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -2,12 +2,17 @@ import streamlit as st
 import pandas as pd
 import folium
 import numpy as np
-from streamlit_folium import folium_static
 
 
 np.random.seed(42)
 boroughs = ['Manhattan', 'Bronx', 'Brooklyn', 'Queens', 'Staten Island']
-neighborhoods = ['Midtown', 'Harlem', 'Williamsburg', 'Astoria', 'St. George']
+neighborhoods = {
+    'Manhattan': ['Midtown', 'Harlem', 'Upper East Side', 'Upper West Side'],
+    'Bronx': ['Riverdale', 'Fordham', 'Bronx Park'],
+    'Brooklyn': ['Williamsburg', 'Park Slope', 'Brooklyn Heights'],
+    'Queens': ['Astoria', 'Long Island City', 'Flushing'],
+    'Staten Island': ['St. George', 'Tompkinsville', 'Stapleton']
+}
 prices = np.random.randint(50, 500, size=100) 
 host_names = ['John', 'Tim', 'Sarah']  
 room_types = ['Private room', 'Entire home/apt', 'Shared room']  
@@ -19,7 +24,7 @@ data = pd.DataFrame({
     'house_name': np.random.choice(['Furnished room in Astoria apartment', 'Single room in Manhattan', 'Cozy apartment in Brooklyn', 'Spacious house in Queens', 'Charming loft in Staten Island'], size=length),  
     'listing_name': np.random.choice(host_names, size=length),
     'borough': np.random.choice(boroughs, size=length),
-    'neighborhood': np.random.choice(neighborhoods, size=length),
+    'neighborhood': np.random.choice(np.concatenate([neighborhoods[borough] for borough in boroughs]), size=length),
     'room_type': np.random.choice(room_types, size=length),
     'price': prices,
     'latitude': np.random.uniform(40.5, 41, size=length),  
@@ -44,21 +49,21 @@ st.write(data.head())
 
 
 st.write('### Instructions')
-st.write('1. Use the dropdowns to select a host and neighborhood.')
+st.write('1. Use the dropdowns to select a borough and neighborhood.')
 st.write('2. Set your desired price range.')
 st.write('3. Explore available housing rentals in the selected area.')
 
 
-hosts = data['listing_name'].unique()
-selected_host = st.selectbox('Select a Host', hosts)
+boroughs = data['borough'].unique()
+selected_borough = st.selectbox('Select a Borough', boroughs)
 
-neighborhoods = data[data['listing_name'] == selected_host]['neighborhood'].unique()
+neighborhoods = data[data['borough'] == selected_borough]['neighborhood'].unique()
 selected_neighborhoods = st.multiselect('Select Neighborhood(s)', neighborhoods)
 
 price_range = st.slider('Price Range', min_value=0, max_value=1000, value=(100, 500))
 
 filtered_data = data[
-    (data['listing_name'] == selected_host) &
+    (data['borough'] == selected_borough) &
     (data['neighborhood'].isin(selected_neighborhoods)) &
     (data['price'].between(price_range[0], price_range[1]))
 ]
@@ -87,7 +92,4 @@ for index, row in filtered_data.iterrows():
         popup=folium.Popup(details, max_width=300, min_width=200)
     ).add_to(m)
 
-folium_static(m)
-
 st.write(m._repr_html_(), unsafe_allow_html=True)
-
